500505eea37b7432cd4ef4b1a7f1667675d2dc6e
kayky233 <1787963736@qq.com>
2021-01-23 21:26:59 +0800

    还是从官方的例子开始改吧，已经得到了.root文件，接下来需要确定追踪哪些参数可以得到能谱

---
+++ B4a/build/CMakeFiles/exampleB4a.dir/DependInfo.cmake
@@ -0,0 +1,54 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/deepmind/mont/worktree/B4/B4a/exampleB4a.cc" "/home/deepmind/mont/worktree/B4/B4a/build/CMakeFiles/exampleB4a.dir/exampleB4a.cc.o"
+  "/home/deepmind/mont/worktree/B4/B4a/src/B4DetectorConstruction.cc" "/home/deepmind/mont/worktree/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4DetectorConstruction.cc.o"
+  "/home/deepmind/mont/worktree/B4/B4a/src/B4PrimaryGeneratorAction.cc" "/home/deepmind/mont/worktree/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4PrimaryGeneratorAction.cc.o"
+  "/home/deepmind/mont/worktree/B4/B4a/src/B4RunAction.cc" "/home/deepmind/mont/worktree/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4RunAction.cc.o"
+  "/home/deepmind/mont/worktree/B4/B4a/src/B4aActionInitialization.cc" "/home/deepmind/mont/worktree/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4aActionInitialization.cc.o"
+  "/home/deepmind/mont/worktree/B4/B4a/src/B4aEventAction.cc" "/home/deepmind/mont/worktree/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4aEventAction.cc.o"
+  "/home/deepmind/mont/worktree/B4/B4a/src/B4aSteppingAction.cc" "/home/deepmind/mont/worktree/B4/B4a/build/CMakeFiles/exampleB4a.dir/src/B4aSteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/deepmind/mont/geant4-install/include/Geant4"
+  "../include"
+  "/home/deepmind/mont/geant4-install/include"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
